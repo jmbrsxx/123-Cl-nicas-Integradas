2a0381e86a9d14623a54f9bf3b995a8af0b78f0d
João Mateus <jmsrs375@gmail.com>
2026-03-24 15:29:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/clinica-site/",
+  base: "/123-Cl-nicas-Integradas/",
   server: {
     host: "::",
     port: 8080,
